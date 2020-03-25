776f877c596a9dabf605fe4a983e050925ff15d0
Jonathan <jon@than.cl>
2020-03-25 13:48:49 -0300

    change environment name

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import ReactGA from "react-ga";
-ReactGA.initialize(process.env.GA_ID || "");
+ReactGA.initialize(process.env.REACT_APP_GA_ID || "");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
