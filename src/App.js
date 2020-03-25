fa37775c3024f503b791eab8ecf97be1989d6ca2
Jonathan <jon@than.cl>
2020-03-25 16:16:06 -0300

    add react-ga

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "./App.css";
 import { Collapse, Button, CardBody, Card } from "reactstrap";
+import ReactGA from "react-ga";
+ReactGA.initialize("G-M48CXR6JD1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
