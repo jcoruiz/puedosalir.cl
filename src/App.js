46899f1bddc1d1d286765e8713e15fc8acf905ed
Jonathan <jon@than.cl>
2020-03-25 13:39:31 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import ReactGA from "react-ga";
+ReactGA.initialize(process.env.GA_ID || "");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
